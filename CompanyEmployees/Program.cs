ab8746f941b5732d042a5f39c9cc0caaf548ce93
enano17711 <120424316+enano17711@users.noreply.github.com>
2023-03-09 14:52:51 -0400

    Logger init

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,22 +1,27 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// ServiceExtension Class
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+// Program Class
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+else app.UseHsts();
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.All });
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
