fc59d04b3a800e17e9065503389389aa0d713037
enano17711 <120424316+enano17711@users.noreply.github.com>
2023-03-09 18:29:07 -0400

    Entities project

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,12 +1,18 @@
 using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// nlog register
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 // Add services to the container.
 // ServiceExtension Class
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
 // Program Class
 builder.Services.AddControllers();
 
