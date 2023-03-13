392207b7e84213b91f17f78fc4a9e8db038c92ac
enano17711 <120424316+enano17711@users.noreply.github.com>
2023-03-13 18:54:32 -0400

    get companies collection from collection body ids

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CompanyEmployees.Presentation.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 
@@ -51,10 +52,14 @@ public class CompaniesController : ControllerBase
     }
 
     // GET COLLECTION
-    [HttpGet("collection/{ids}", Name = "CompanyCollection")]
-    public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+    [HttpGet("collection/{ids}",
+        Name = "CompanyCollection")]
+    public IActionResult GetCompanyCollection(
+        [ModelBinder(BinderType = typeof(ArrayModelBinder))]
+        IEnumerable<Guid> ids)
     {
-        var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
+        var companies = _service.CompanyService.GetByIds(ids,
+            trackChanges: false);
 
         return Ok(companies);
     }
@@ -65,6 +70,11 @@ public class CompaniesController : ControllerBase
     {
         var result = _service.CompanyService.CreateCompanyCollection(companyCollection);
 
-        return CreatedAtRoute("CompanyCollection", new { result.ids }, result.companies);
+        return CreatedAtRoute("CompanyCollection",
+            new
+            {
+                result.ids
+            },
+            result.companies);
     }
 }
